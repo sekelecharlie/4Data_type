e83ac8ae21f5ae3bc5e1cc8e27f88b16eff9b9ea
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-11 10:58:23 +0200

    Add project files.

---
+++ 4Data_type/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Data_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration of variables
+            string name = "Charlie";
+            int age = 28;
+            bool isTall = true;
+            double weight = 68.1;
+            char initial = 'C';
+            Console.WriteLine("His name is " + name + " he is " + age + " it is " + isTall + " that he is tall");
+            Console.WriteLine("His weight in Kgs is " + weight + " his initials is " + initial);
+
+
+            name = "Thato";
+            age = 25;
+            isTall = true;
+            weight = 55.8;
+            initial = 'T';
+            Console.WriteLine("His name is " + name + " he is " + age + " it is " + isTall + " that he is tall");
+            Console.WriteLine("His weight in Kgs is " + weight + " his initials is " + initial);
+
+            //freeze the console
+            Console.ReadLine ();
+
+
+        }
+    }
+}
